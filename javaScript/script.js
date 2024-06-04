15733992de5cfe498d2c6623dc72a386ac59d043
Rubinho <rubens.rgsa@gmail.com>
2024-06-03 23:34:39 -0300

    Corrçeões
    aba produtos responsividades

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,11 +7,27 @@ $(document).ready(function(){
         
     });
     $("#promocao").owlCarousel({
-        items: 5,
-        nav: true,
         // loop: true,
-        margin: 20
-        
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                margin: 20
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1024: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            }
+        }
     });
     
 }); 
